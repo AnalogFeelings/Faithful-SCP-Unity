6f24e1f960ae76371e0aca1214e6ea308d08b678
ricky-daniel13 <47061347+ricky-daniel13@users.noreply.github.com>
2019-03-15 16:43:24 -0600

    Update and rename Object_Button.cs to Object_Interact.cs

--- Assets/Scripts/Objects/Object_Interact.cs
+++ Assets/Scripts/Objects/Object_Interact.cs
@@ -1,21 +1,19 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Object_Button : MonoBehaviour
+// :{)
+
+public abstract class Object_Interact : MonoBehaviour
 {
-    public GameObject Door01, Door02;
-    public int function = 0;
+    /*public GameObject Door01, Door02;
     float deactivate;
     public bool activated;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         if (function == 1)
         {
@@ -23,11 +21,12 @@ public class Object_Button : MonoBehaviour
             if (deactivate <= 0)
                 activated = false;
         }
-    }
+    }*/
 
-    public void Pressed()
+    public virual void Pressed()
     {
-        if (function == 0)
+        
+        /*if (function == 0)
         {
             Door01.GetComponent<Object_Door>().DoorSwitch();
             if (Door02 != null)
@@ -37,6 +36,23 @@ public class Object_Button : MonoBehaviour
         {
             deactivate = 1.0f;
                 activated = true;
+        }*/
+    }
+    
+    public virual void Hold()
+    {
+    
+        //¿Que pasa si sostengo el boton?
+        /*if (function == 0)
+        {
+            Door01.GetComponent<Object_Door>().DoorSwitch();
+            if (Door02 != null)
+                Door02.GetComponent<Object_Door>().DoorSwitch();
         }
+        if (function == 1)
+        {
+            deactivate = 1.0f;
+                activated = true;
+        }*/
     }
 }
