3e26d3aecff8d58e274cbc8541f518bad854f76d
Ricardo <danykun.echidna@gmail.com>
2019-03-19 23:46:37 -0600

    Revert "Revert "Avance Items""
    This reverts commit 300cf85ff72975678d17361b4a7ef51fefdf4145.

--- Assets/Scripts/Player/slotController.cs
+++ Assets/Scripts/Player/slotController.cs
@@ -2,31 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class slotController : MonoBehaviour
+public class slotController : MonoBehaviour, IDragHandler, IEndDragHandler
 {
+    Vector3 orpos;
+    bool dragging;
     public Item item;
-    public Text displayText;
-    public Image displayImage;
+    public int id;
+    public bool isEquip;
 
     private void Start()
     {
         updateInfo();
+        orpos = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+        dragging = true;
+        ItemController.instance.currdrag = id;
+        GetComponent<Image>().raycastTarget = false;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+ 
+        if (ItemController.instance.slots[ItemController.instance.currhover].item == null)
+            slotMove();
+
+        transform.position = orpos;
+        GetComponent<Image>().raycastTarget = true;
+        dragging = false;
     }
 
 
 
     public void updateInfo()
     {
+        Text displayText = transform.Find("Text").GetComponent<Text>();
+        Image displayImage = transform.Find("Image").GetComponent<Image>();
+
         if (item)
         {
-            displayText.text = item.itemDispName;
+            if (!isEquip)
+                displayText.text = item.itemDispName;
+            else
+                displayText.text = "Using " + item.itemDispName;
             displayImage.sprite = item.icon;
             displayImage.color = Color.white;
         }
         else
         {
-            displayText.text = "";
+            displayText.text = " " + id;
             displayImage.sprite = null;
             displayImage.color = Color.black;
         }
@@ -35,10 +63,30 @@ public class slotController : MonoBehaviour
 
     public void Use()
     {
-        if (item)
+        if (item && !dragging)
         {
-            Debug.Log("You clicked: " + item.itemName);
+            item.Use();
+            if (item.deleteUse == true)
+            {
+                item = null;
+            }
+
         }
+        updateInfo();
     }
+
+    public void Hover()
+    {
+        ItemController.instance.currhover = id;
+        Debug.Log(id);
+    }
+    public void slotMove()
+    {
+        ItemController.instance.slots[ItemController.instance.currhover].item = item;
+        ItemController.instance.slots[ItemController.instance.currhover].updateInfo();
+        item = null;
+        updateInfo();
+    }
+
 
 }
